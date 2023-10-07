1389601b72c41aa5df4117f4a149379e9e192d1b
yamaan93 <yamaan93@gmail.com>
2023-10-07 10:45:33 -0400

    setup FreeRTOS and simple CAN sniffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,68 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
+#include "arduino_freertos.h"
+#include "avr/pgmspace.h"
+
+#define Serial if(Serial)Serial //fixes issue where prints in ISR hangs the program, might wanna look into a threadsafe alternative though
 
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> Can0;
-// put function declarations here:
-void canSniff(const CAN_message_t &msg);
 
+static void task1(void*) {
+    while (true) {
+        digitalWriteFast(LED_BUILTIN, LOW);
+        vTaskDelay(pdMS_TO_TICKS(500));
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("plz bro");
-  Can0.begin();
-  Can0.setBaudRate(500000);
-  Can0.setMaxMB(16);
-  Can0.enableFIFO();
-  Can0.enableFIFOInterrupt();
-  Can0.onReceive(canSniff);
-  Can0.mailboxStatus();
+        digitalWriteFast(LED_BUILTIN, HIGH);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
 
-void loop() {
-  Can0.events();
-  static uint32_t timeout = millis();
-  if ( millis() - timeout > 200 ) {
-    CAN_message_t msg;
-    msg.id = random(0x1,0x7FE);
-    for ( uint8_t i = 0; i < 8; i++ ) msg.buf[i] = i + 1;
-    Can0.write(msg);
-    timeout = millis();
-  }
+static void task2(void*) {
+    while (true) {
+        Serial.println("TICK");
+        vTaskDelay(pdMS_TO_TICKS(1'000));
 
-  delay(100);
+        Serial.println("TOCK");
+        vTaskDelay(pdMS_TO_TICKS(1'000));
+    }
 }
-
-
 void canSniff(const CAN_message_t &msg) {
-  Serial.print("MB "); Serial.print(msg.mb);
-  Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
-  Serial.print("  LEN: "); Serial.print(msg.len);
-  Serial.print(" EXT: "); Serial.print(msg.flags.extended);
-  Serial.print(" TS: "); Serial.print(msg.timestamp);
-  Serial.print(" ID: "); Serial.print(msg.id, HEX);
-  Serial.print(" Buffer: ");
-  for ( uint8_t i = 0; i < msg.len; i++ ) {
-    Serial.print(msg.buf[i], HEX); Serial.print(" ");
-  } Serial.println();
+//   Serial.print(msg.id);
+//   Serial.print(" ");
+//   for ( uint8_t i = 0; i < msg.len; i++ ) {
+//     Serial.print(msg.buf[i]); Serial.print(" ");
+//   } Serial.println();
 }
+
+
+FLASHMEM __attribute__((noinline)) void setup() {
+    
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWriteFast(LED_BUILTIN, HIGH);
+
+    delay(5'000);
+
+    if (CrashReport) {
+        Serial.print(CrashReport);
+        Serial.println();
+        Serial.flush();
+    }
+    Can0.begin();
+    Can0.setBaudRate(500000);
+    Can0.setMaxMB(64);
+    Can0.enableFIFO();
+    Can0.enableMBInterrupt(FIFO);
+    Can0.enableFIFOInterrupt();
+    Can0.onReceive(canSniff);
+    Serial.println(PSTR("\r\nBooting FreeRTOS kernel " tskKERNEL_VERSION_NUMBER ". Built by gcc " __VERSION__ " (newlib " _NEWLIB_VERSION ") on " __DATE__ ". ***\r\n"));
+
+    xTaskCreate(task1, "task1", 128, nullptr, 2, nullptr);
+    xTaskCreate(task2, "task2", 128, nullptr, 2, nullptr);
+
+    Serial.println("setup(): starting scheduler...");
+    Serial.flush();
+
+    vTaskStartScheduler();
+}
+
+void loop() {}
