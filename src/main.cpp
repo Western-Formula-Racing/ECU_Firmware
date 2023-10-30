80668fd8637a6a9b8febfd27197cffe0742e085e
yamaan93 <yamaan93@gmail.com>
2023-10-29 22:34:55 -0400

    update submodule

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,21 @@
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> Can0;
 FS_CAN FS_CAN0(&Can0); 
 static void task1(void*) {
+    uint32_t testData = 0;
+    uint32_t testData2 = 0;
     FS_CAN::CAN_MSG testMsg;
     testMsg.id = 256;
     testMsg.signal_count = 1;
-    FS_CAN::CAN_SIGNAL testSignal{&testMsg, 0, 8};
+    FS_CAN::CAN_SIGNAL testSignal{&testData, 0, 16};
+    FS_CAN::CAN_SIGNAL testSignal2{&testData2, 8, 8};
+    testMsg.signals.push_back(&testSignal);
+    testMsg.signals.push_back(&testSignal2);
     Serial.printf("didnt' crash yet\n");
     FS_CAN0.subscribe_to_message(&testMsg);
     while (true) {
         digitalWriteFast(LED_BUILTIN, LOW);
         vTaskDelay(pdMS_TO_TICKS(500));
-
+        Serial.printf("data1:%d data2:%d\n", testData, testData2);
         digitalWriteFast(LED_BUILTIN, HIGH);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
