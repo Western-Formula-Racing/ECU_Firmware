89e24bda0066d4ff1720589360ccfd67342bd75d
yamaan93 <yamaan93@gmail.com>
2023-11-14 20:24:14 -0500

    add message queue to task2

---
+++ include/main.h
@@ -0,0 +1,14 @@
+enum LogLevel
+{
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+};
+
+typedef struct
+{
+    // The log level of the message
+    LogLevel level;
+    // Fixed length of 256 characters
+    char message[256];
+} LogMessage_t;
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,15 @@
 #include "FS_CAN.h"
 #include "interfaces/inverter.h"
 #include "interfaces/bms.h"
+#include "main.h"
 #pragma GCC optimize ("O0")
 
 //#define Serial if(Serial)Serial //fixes issue where prints in ISR hangs the program, might wanna look into a threadsafe alternative though
-
+void log_message(LogLevel level, const char *message);
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> Can0;
 FS_CAN FS_CAN0(&Can0); 
+static QueueHandle_t logQueue = NULL;
+
 static void task1(void*) {
     uint32_t testData = 0;
     uint32_t testData2 = 0;
@@ -24,21 +27,32 @@ static void task1(void*) {
     testMsg.signals.push_back(&testSignal2);
     Serial.printf("didnt' crash yet\n");
     FS_CAN0.subscribe_to_message(&testMsg);
+    logQueue = xQueueCreate(50, sizeof(LogMessage_t));
     while (true) {
         digitalWriteFast(LED_BUILTIN, LOW);
         vTaskDelay(pdMS_TO_TICKS(500));
         Serial.printf("data1:%d data2:%d\n", testData, testData2);
+        char str [20];
+        sprintf(str,"Hello from task1");
+        log_message(LOG_INFO, str);
         digitalWriteFast(LED_BUILTIN, HIGH);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
 static void task2(void*) {
+    //vTaskDelay(pdMS_TO_TICKS(5000));
     while (true) {
-        Serial.println("TICK");
-        vTaskDelay(pdMS_TO_TICKS(1'000));
+        
 
-        //Serial.println("TOCK");
+        if (logQueue != NULL){
+            LogMessage_t msg;
+        if (xQueueReceive(logQueue, &msg, 0) == pdTRUE)
+        {
+            Serial.println("black_box(): received message from queue");
+            Serial.printf("%d: %s\n", msg.level, msg.message);
+        }
+        }
         vTaskDelay(pdMS_TO_TICKS(1'000));
     }
 }
@@ -46,13 +60,38 @@ void canSniff(const CAN_message_t &msg) {
   FS_CAN0.CAN_RX_ISR(msg);
 }
 
+void log_message(LogLevel level, const char *message)
+{
+    LogMessage_t msg;
+    msg.level = level;
+
+    strncpy(msg.message, message, 256);
+
+    // Ensure the message is null terminated
+    if (sizeof(msg.message) > 0)
+    {
+        msg.message[sizeof(msg.message) - 1] = 0;
+    }
+
+    if (xQueueSendToBack(logQueue, &msg, portMAX_DELAY) == pdPASS)
+    {
+        Serial.println("log_message(): sent message to queue");
+    }
+    else
+    {
+        Serial.println("log_message(): failed to send message to queue");
+    }
+}
+
+
+
 
 FLASHMEM __attribute__((noinline)) void setup() {
 
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWriteFast(LED_BUILTIN, HIGH);
     
-    delay(5'000);
+    //Serial.begin(96000);
 
     if (CrashReport) {
         Serial.print(CrashReport);
@@ -69,8 +108,8 @@ FLASHMEM __attribute__((noinline)) void setup() {
     Serial.println(PSTR("\r\nBooting FreeRTOS kernel " tskKERNEL_VERSION_NUMBER ". Built by gcc " __VERSION__ " (newlib " _NEWLIB_VERSION ") on " __DATE__ ". ***\r\n"));
 
     xTaskCreate(task1, "task1", 5028, nullptr, 2, nullptr);
-    xTaskCreate(task2, "task2", 128, nullptr, 2, nullptr);
-
+    xTaskCreate(task2, "task2", 1028, nullptr, 2, nullptr);
+    delay(10000);
     Serial.println("setup(): starting scheduler...");
     Serial.flush();
 
