69aa721bb6bb339b8ace9e295481f98a83ad2baa
Collin Town <35181633+towner-10@users.noreply.github.com>
2024-01-25 13:05:31 -0500

    Proper declaration of PDM implementation

--- src/interfaces/pdm.cpp
+++ src/interfaces/pdm.cpp
@@ -1,18 +1,18 @@
 #include "interfaces/pdm.h"
 
-namespace PDM {
-    void PDM::begin() {
-        pinMode(HSDIN6, OUTPUT);
-        pinMode(HSDIN5, OUTPUT);
-        pinMode(HSDIN4, OUTPUT);
-        pinMode(HSDIN2, OUTPUT);
-        pinMode(HSDIN7, OUTPUT);
-        pinMode(HSDIN8, OUTPUT);
-        pinMode(HSDIN1, OUTPUT);
-        pinMode(HSDIN3, OUTPUT);
-    }
+void PDM::begin()
+{
+    pinMode(HSDIN6, OUTPUT);
+    pinMode(HSDIN5, OUTPUT);
+    pinMode(HSDIN4, OUTPUT);
+    pinMode(HSDIN2, OUTPUT);
+    pinMode(HSDIN7, OUTPUT);
+    pinMode(HSDIN8, OUTPUT);
+    pinMode(HSDIN1, OUTPUT);
+    pinMode(HSDIN3, OUTPUT);
+}
 
-    void PDM::setPin(PDM_PINS pin, bool state) {
-        digitalWriteFast(pin, state);
-    }
+void PDM::setPin(PDM_PINS pin, bool state)
+{
+    digitalWriteFast(pin, state);
 }
