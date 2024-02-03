d90092609a78035c595443e748a2a8d22f1f5699
yamaan93 <yamaan93@gmail.com>
2024-02-03 17:27:47 -0500

    remove fitler for button, uneeded

--- src/digitalSensor.cpp
+++ src/digitalSensor.cpp
@@ -1,11 +1,12 @@
 #include "digitalSensor.h"
 
-DigitalSensor::DigitalSensor(ADC *adc_handle, uint8_t pin) : Sensor{adc_handle, pin, 0,1,2,1,1e3,true}{
+DigitalSensor::DigitalSensor(ADC *adc_handle, uint8_t pin) : Sensor{adc_handle, pin, 0,1,2,100,1e3,true}{
 
 }
 
 void DigitalSensor::read(){
     value = static_cast<float>(digitalRead(pin));
     Serial.printf(">RawButton:%d\n", static_cast<int>(value));
-    filteredValue = filter.filt(value);
+    //button doesn't really seem to need filtering/debouncing
+    //filteredValue = filter.filt(value);
 }
